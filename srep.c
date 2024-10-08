2bb52634d0967f8bb5bebc9a17ba5c8797589a6a
Nicolas Gajardo Ponce <nicolasgajardo11@gmail.com>
2024-10-08 00:12:09 -0300

    Srep funcionando solo con char

--- srep.c
+++ srep.c
@@ -3,12 +3,24 @@
 #include <unistd.h>
 #include <getopt.h>
 
+char* reemplazarCaracter(char *linea, char caracterAntiguo, char caracterNuevo) {
+    for (int i = 0; linea[i] != '\0'; i++) {
+        if (linea[i] == caracterAntiguo) {
+            linea[i] = caracterNuevo;
+        }
+    }
+    //printf(linea);
+    return linea;
+}
+
+
+
 // Entradas : Recibe el archivo de entrada
 // Salida :
 // Descripción : Recibe un archivo y lo procesa
-void procesar_archivo(char* filename) {
+void procesar_archivo(char* filename, char caracterAntiguo, char caracterNuevo) {
     int contador = 0;
-    char line[256];
+    char linea[1024];
 
     // Abrir el archivo en modo lectura
     FILE* file = fopen(filename, "r");  
@@ -18,10 +30,13 @@ void procesar_archivo(char* filename) {
     }
 
     //Leer linea por linea el archivo
-    while (fgets(line, sizeof(line), file)){
-        printf("%s", line);
+    while (fgets(linea, sizeof(linea), file)){
+        char *uwu = reemplazarCaracter(linea, caracterAntiguo, caracterNuevo);
+        printf("%s", uwu);
         contador++;
     }
+
+
 
     printf("\nNúmero total de líneas: %d\n", contador);
 
@@ -36,17 +51,25 @@ int main(int argc, char *argv[])
     int option;
     char *archivoentrada = NULL;
     char *archivosalida = NULL;
-    char *separador = NULL;
-    char *columnas = NULL;
+    char *nuevo = NULL;
+    char *antiguo = NULL;
+    char a;
+    char n;
 
     //Se utiliza geopt para leer las opciones de línea de comandos
     while ((option = getopt(argc, argv, "i:o:s:S:")) != -1) {
         switch (option) {
             case 's':
-                separador = optarg; //carácter separador de la columnas
+                if (optarg && optarg[0] != '\0') {
+                    //antiguo = optarg[0]; // Obtener el primer carácter
+                    a = optarg[0];
+                }
                 break;
             case 'S':
-                columnas = optarg; //Indicar la o las columnas 
+                if (optarg && optarg[0] != '\0') {
+                    //nuevo = optarg[0]; // Obtener el primer carácter
+                    n = optarg[0];
+                }
                 break;
             case 'i':
                 archivoentrada = optarg; //Nombre del archivo de entrada
@@ -55,7 +78,7 @@ int main(int argc, char *argv[])
                 archivosalida = optarg; //Nombre del archivo de salida
                 break;
             default:
-                fprintf(stderr, "Uso: %s [-i archivoentrada] [-o archivosalida] [-d separador] [-c columnas]\n", argv[0]);
+                fprintf(stderr, "Uso: %s [-i archivoentrada] [-o archivosalida] [-s antiguo] [-S nuevo]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
@@ -66,7 +89,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    procesar_archivo(archivoentrada);
+    procesar_archivo(archivoentrada, a, n);
 
     printf("*****************FIN DEL PROGRAMA*****************\n");
     return 0;
