1f80f57c87c84ddf4b315a8eaaf0eb3a4b797f2a
cristianromero97 <cristian.romero.m@usach.cl>
2024-10-11 15:02:33 -0400

    count listo

--- count.c
+++ count.c
@@ -2,80 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include "funcionescount.c"
 
-int main(int argc, char *argv[])
-{
-    printf("****************INICIO DEL PROGRAMA***************\n\n");
+//Bloque Principal
+int main(int argc, char *argv[]) {
+    printf("****************INICIO DEL PROGRAMA****************\n");
 
-    // Leer argumentos de la línea de comandos
-    int option;
+    // Variables para almacenar las opciones de línea de comandos
     char *archivoentrada = NULL;
-    char *archivosalida = "output.txt";
     char cantidad_lineas = 0;
     char cantidad_caracteres = 0;
-    FILE *archivo;
-
-    //Se utiliza geopt para leer las opciones de línea de comandos
-    while ((option = getopt(argc, argv, "i:CL")) != -1) {
-        switch (option) {
-            case 'i':
-                archivoentrada = optarg; //Nombre del archivo de entrada
-                break;
-            case 'C':
-                cantidad_caracteres = 1; //Contar el número de caracteres
-                break;
-            case 'L':
-                cantidad_lineas = 1;//Contar el numero de líneas
-                break;
-            default:
-                fprintf(stderr, "Uso: %s [-i archivoentrada] [-L][-C]\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
-    }
-
-    //Manejo de errores en el archivo
-    if (archivoentrada == NULL){
-        fprintf(stderr, "Debe ingresar el nombre para el archivo de entrada (-i). \n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Abrir el archivo de entrada
-    archivo = fopen(archivoentrada, "r");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo de entrada");
-        exit(EXIT_FAILURE);
-    }
-    
     int caracteres = 0;
     int lineas = 0;
-    char c;
 
-    while((c=fgetc(archivo))!=EOF){
-        if(cantidad_caracteres){
-            caracteres++;
-        }
-        /*if(cantidad_lineas && c == '\n'){
-            lineas++;
-        }*/
-    }
+    // Llamado de la funcion opciones
+    opciones(argc, argv, &archivoentrada, &cantidad_caracteres, &cantidad_lineas);
 
-    while((caracteres=getchar())!=EOF){
-        caracteres=cantidad_lineas;
-        if(caracteres=='\n'){
-            lineas++;
-        }
-    }
+    // Abrir el archivo de entrada
+    FILE *archivo = procesar_archivo(archivoentrada);
 
+    // Llamado de la funcion calculo
+    calculo(archivo, cantidad_caracteres, cantidad_lineas, &caracteres, &lineas);
+
+    // Cierre de archivo
     fclose(archivo);
 
-     // Imprimo los resultados dependiendo de las opciones pasadas
-    if (cantidad_lineas && cantidad_caracteres) {
-        printf("%d %d\n", lineas, caracteres);  // Si se pasa -L y -C, imprime líneas seguido de caracteres
-    } else if (cantidad_lineas) {
-        printf("%d\n", lineas);  // Solo líneas si se pasa -L
-    } else if (cantidad_caracteres) {
-        printf("%d\n", caracteres);  // Solo caracteres si se pasa -C
-    }
-    printf("\n*****************FIN DEL PROGRAMA*****************\n");
+    // Llamado de la funcion resultados
+    resultados(lineas, caracteres, cantidad_lineas, cantidad_caracteres);
+
+    printf("*****************FIN DEL PROGRAMA*****************\n");
     return 0;
 }
--- funcionescount.c
+++ funcionescount.c
@@ -3,10 +3,75 @@
 #include <unistd.h>
 #include <getopt.h>
 
-int main(int argc, char *argv[])
-{
-    printf("****************INICIO DEL PROGRAMA****************\n");
+// Función opciones
+// Entradas : Recibe los comandos de entrada
+// Salida : void
+// Descripción : Recibe los comandos de entrada, [-i archivoentrada] [-L] [-C] para realizar la lectura de comandos.
+void opciones(int argc, char *argv[], char **archivoentrada, char *cantidad_caracteres, char *cantidad_lineas) {
+    int option;
+    while ((option = getopt(argc, argv, "i:CL")) != -1) {
+        switch (option) {
+            case 'i':
+                *archivoentrada = optarg; // Nombre del archivo de entrada
+                break;
+            case 'C':
+                *cantidad_caracteres = 1; // Contar el número de caracteres
+                break;
+            case 'L':
+                *cantidad_lineas = 1; // Contar el número de líneas
+                break;
+            default:
+                fprintf(stderr, "Uso: %s [-i archivoentrada] [-L] [-C]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
-    printf("*****************FIN DEL PROGRAMA*****************\n");
-    return 0;
+    //Manejo de errores en el archivo
+    if (*archivoentrada == NULL) {
+        fprintf(stderr, "Debe ingresar el nombre para el archivo de entrada (-i).\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Función procesar_archivo
+// Entradas : Recibe como char el nombre del archivo.
+// Salida : archivo.
+// Descripción : Funcion que permite abrir un archivo en modo "r".
+FILE* procesar_archivo(char *archivoentrada) {
+    FILE *archivo = fopen(archivoentrada, "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo de entrada");
+        exit(EXIT_FAILURE);
+    }
+    return archivo;
+}
+
+// Función calculo
+// Entradas : Recibe el archivo, cantidad_caracteres(char), cantidad_lineas(char), caracteres(int), lineas(int)
+// Salida : void.
+// Descripción : Funcion que calcula la cantidad de caracteres de un archivo y la cantidad de lineas que posee el mismo archivo.
+void calculo(FILE *archivo, char cantidad_caracteres, char cantidad_lineas, int *caracteres, int *lineas) {
+    char c;
+    while ((c = fgetc(archivo)) != EOF) {
+        if (cantidad_caracteres) {
+            (*caracteres)++;
+        }
+        if (cantidad_lineas && c == '\n') {
+            (*lineas)++;
+        }
+    }
+}
+
+// Función resultados
+// Entradas : Recibe como variable: lineas(int), caracteres(int), cantidad_lineas(char), cantidad_caracteres(char)
+// Salida : void.
+// Descripción : Funcion que imprime los resultados
+void resultados(int lineas, int caracteres, char cantidad_lineas, char cantidad_caracteres) {
+    if (cantidad_lineas && cantidad_caracteres) {
+        printf("%d %d\n", lineas, caracteres);   //Imprimir la cantidad de líneas y caracteres
+    } else if (cantidad_lineas) {
+        printf("%d\n", lineas); // Imprimir la cantidad de líneas
+    } else if (cantidad_caracteres) {
+        printf("%d\n", caracteres); // Imprimir la cantidad de caracteres
+    }
 }
